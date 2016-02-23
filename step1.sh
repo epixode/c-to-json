13a61001a7c282e6c6e133debbef1d7c4511a008
Sebastien Carlier <s.carlier@epixode.fr>
2016-02-23 13:22:52 +0000

    run step1 within a container

--- step1.sh
+++ step1.sh
@@ -2,7 +2,11 @@
 set -e
 
 DIR="$( cd "$(dirname $0)" && pwd )"
-BUILD_DIR="${DIR}/build"
+test "$1" = "--in-container" || exec docker run --rm --interactive --tty \
+	--volume "$DIR:/mnt" llvm-build-env /mnt/step1.sh --in-container
+
+BUILD_DIR="$DIR/build"
+mkdir -p "${BUILD_DIR}"
 
 if test -d "${BUILD_DIR}/jsoncpp"; then
 	(cd "${BUILD_DIR}/jsoncpp" && git pull)
@@ -13,6 +17,4 @@ fi
 svn co "http://llvm.org/svn/llvm-project/llvm/trunk" "${BUILD_DIR}/llvm"
 svn co "http://llvm.org/svn/llvm-project/cfe/trunk" "${BUILD_DIR}/llvm/tools/clang"
 svn co "http://llvm.org/svn/llvm-project/compiler-rt/trunk" "${BUILD_DIR}/llvm/projects/compiler-rt"
-
-docker build --tag llvm-build-env llvm-build-env
 
