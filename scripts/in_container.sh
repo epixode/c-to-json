561ebcde428aa871e4b25690c79dff7e39e7bb6e
Sebastien Carlier <s.carlier@epixode.fr>
2016-02-23 14:26:42 +0000

    fix file ownership by running container as user

--- scripts/in_container.sh
+++ scripts/in_container.sh
@@ -1,6 +1,10 @@
-SCRIPT=$(basename "$0")
 DIR="$( cd "$(dirname "$0")" && pwd )"
-test "$IN_CONTAINER" || exec docker run --rm --interactive --tty \
-  --volume "$DIR:/mnt" build_c-to-json "/mnt/$SCRIPT"
+
+if test -z "$IN_CONTAINER"; then
+  UID=$(id -u)
+  SCRIPT=$(basename "$0")
+  exec docker run --rm --interactive --tty --user $(id -u) \
+    --volume "$DIR:/mnt" build_c-to-json "/mnt/$SCRIPT"
+fi
 
 BUILD_DIR="/mnt/build"
