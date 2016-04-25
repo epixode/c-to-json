28fd6150748393c86d012626953ff71341fe1578
Sebastien Carlier <s.carlier@epixode.fr>
2016-04-25 10:46:20 +0000

    in_container: create and switch to user with correct UID

--- scripts/in_container.sh
+++ scripts/in_container.sh
@@ -1,10 +1,13 @@
 DIR="$( cd "$(dirname "$0")" && pwd )"
 
 if test -z "$IN_CONTAINER"; then
-  UID=$(id -u)
   SCRIPT=$(basename "$0")
-  exec docker run --rm --interactive --tty --user $(id -u) \
+  exec docker run --rm --interactive --tty --env RUN_UID=$(id -u) \
     --volume "$DIR:/mnt" build_c-to-json "/mnt/$SCRIPT"
+elif test 0 -eq $(id -u); then
+  echo id $RUN_UID script $0
+  echo "user::$RUN_UID:0:,,,:/:/bin/bash" >>/etc/passwd
+  exec su -c "$0" user
 fi
 
 BUILD_DIR="/mnt/build"
