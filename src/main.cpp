3107a42824d43afbdf588b5e874a48bdef9b9274
Marc Jeanmougin <marc@jeanmougin.fr>
2018-12-19 12:11:49 +0100

    Fix compilation and link errors

--- src/main.cpp
+++ src/main.cpp
@@ -483,7 +483,7 @@ void parseTranslationUnit(void *data) {
     /*CacheCodeCompletionResults*/ false,
     /*IncludeBriefCommentsInCodeCompletion*/ false,
     /*AllowPCHWithCompilerErrors=*/ false,
-    /*SkipFunctionBodies*/ false,
+    /*SkipFunctionBodies*/ clang::SkipFunctionBodiesScope::None,
     /*SingleFileParse*/ true,
     /*UserFilesAreVolatile*/ true,
     /*ForSerialization*/ false,
